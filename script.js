10ae9b0fdb29155123cb56539b068cebabe48ea7
paweenut0964 <pwirnuchw@gmail.com>
2026-01-28 17:16:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function showAlert(){
 alert("alert Message")
+
+}
+
+function toggleAbout() {
+    const about = document.getElementById("aboutMe");
+
+    if (about.style.display === "none" || about.style.display === "") {
+        about.style.display = "flex";
+    } else {
+        about.style.display = "none";
+    }
 }
